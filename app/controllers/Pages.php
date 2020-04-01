963fafeae52843359a7405f72d16d96ddb32b04d
unknown <j0h@example.com>
2020-04-01 16:19:37 +0300

    adding loading views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,11 +5,24 @@
 		}
 
 		public function index(){ //sis metodas reikalingas, kad uzkrovus puspapi nebutu erroro
+			
 			//$this->view('hello'); // get answer in browser View does not exist, because not hello.php inside views
+
+			//$this->view('pages/index'); //uzkraus HOMEPAGE nes padavem i view index.php faila
+			
+			//$this->view('pages/index', ['title' => 'Welcome']);//pass data to array
+
+			$data = [
+				'title' => 'Welcome'
+			];
+
+			$this->view('pages/index', $data); //masyva iskelem atskirai, padavem tik kitamaji
+
 		}
 
-		public function about($id){
-			echo $id;
+		public function about(){ //id uzkomentuojamas
+			//echo $id;
+			$this->view('pages/about'); //http://localhost/oop/mvc/pages/about gausime ABOUT
 		}
 	}
 
---
+++ app/views/pages/index.php
@@ -0,0 +1,1 @@
+<h1> <?php echo $data['title']; ?> </h1> <!-- http://localhost/oop/mvc/     gausime welcome  jis gautas controrellio pagalba-->
